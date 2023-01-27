308f4b8e9382b89ac695e80e55e4957f6665d317
1343744768 <79694861+1343744768@users.noreply.github.com>
2023-01-27 20:42:43 +0800

    Update utils_fit.py

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -139,8 +139,8 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
         if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
             torch.save(model.state_dict(), os.path.join(save_dir, 'exp-%03d-train_loss%.3f-val_loss%.3f.pth'%((epoch + 1), total_loss / epoch_step, val_loss / epoch_step_val)))
 
-        if eval_callback.period != 1:  # 如果不每轮验证，保存最好模型以val loss最小且f1得分最大为标准
-            if (val_f_score / epoch_step_val) > 0.5 and (val_f_score / epoch_step_val) >= max(loss_history.f_score) and (val_loss / epoch_step_val) <= min(loss_history.val_loss):
+        if eval_callback.period != 1:  # 如果不每轮验证，保存最好模型以val f1得分最大为标准
+            if (val_f_score / epoch_step_val) > 0.5 and (val_f_score / epoch_step_val) >= max(loss_history.f_score):
                 print('Save best model to best_epoch_weights.pth')
                 torch.save(model.state_dict(), os.path.join(save_dir, "best_weights.pth"))
         else:   # 如果每轮验证，则保存最好模型以miou最大为标准
